2abecdf1579a3029b9ac14f17ad1646168eb91f7
ClemsRobot <cle.castel@gmail.com>
2018-10-18 21:14:42 +0200

    push

--- app.rb
+++ app.rb
@@ -1,22 +1,20 @@
-<<<<<<< HEAD
-load 'lib/app/get_url.rb'
-load 'lib/app/scraper.rb'
-=======
->>>>>>> d5be93983581d134f5f7c024852674ac303fefb4
-# frozen_string_literal: true
 
 require 'rubygems'
 require 'nokogiri'
+require 'open-uri'
+require 'json'
+require 'pp'
+require "google_drive"
 require 'csv'
 require 'dotenv'
-require 'json'
-require "google_drive"
-require 'open-uri'
 Dotenv.load("../../.env")
 
+load "lib/app/scrapper.rb"
+load "lib/app/csv_adder.rb"
 load "lib/app/townhalls_adder_to_db.rb"
-load "lib/app/townhalls_follower.rb"
 load "lib/app/townhalls_mailer.rb"
-load "lib/app/townhalls_scrapper.rb"
+load "lib/app/townhalls_follower.rb"
 load "lib/views/index.rb"
 load "lib/views/done.rb"
+
+Index.new.launch
